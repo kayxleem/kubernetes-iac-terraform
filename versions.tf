2f494fe4bab1fe64ad9b17d497cf04ba98c821a4
Adegbuyi Oluwakayode <kayode_adegbuyi@yahoo.com>
2023-03-20 23:00:58 +0100

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.47"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.17"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.8"
+    }
+      grafana = {
+      source  = "grafana/grafana"
+      version = ">= 1.34"
+    }
+  }
+
+
+}
